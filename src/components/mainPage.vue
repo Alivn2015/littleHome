5c82a6363b2e4b25b47ff0dbbbe4e510a5324a0a
Alvin2015 <wengangjiang@126.com>
2020-05-06 16:02:41 +0800

    dist更新试一试

---
+++ src/components/mainPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="main-page">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'mainPage',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.main-page {
+  width: 100%;
+  height: 100px;
+  background-color: rgb(207, 134, 134);
+}
+</style>
